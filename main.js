bf32a919fba86b5875e4c5791ab8dc81825b664e
Mason Goddard <Business.goddardm@gmail.com>
2026-04-14 08:42:00 -0400

    Added mobile style compatibility.

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function highlightEl(el){
 
 // Function for Project descrioptions
     
-const all_desc = document.getElementById("projs").querySelectorAll(".desc")
+const all_desc = document.getElementById("projs").querySelectorAll(".desc");
 
 function viewProj(el){
 
@@ -36,41 +36,5 @@ function viewProj(el){
     const content = ul.parentElement;
 
     content.classList.toggle('view');
-
-}
-
-
-// Function for view Course subjects
-
-const all_view = document.getElementById("courses").querySelectorAll(".course")
-
-function viewCourse(el){
-
-
-    Array.from(all_view).forEach(l => {
-        console.log(l);
-        if (el != l){
-
-
-            let cont = l.nextElementSibling;
-            let icon = l.lastElementChild;
-
-            console.log(cont, icon);
-
-
-            cont.classList.remove('view');
-            icon.classList.remove('view');
-
-
-        }
-    })
-
-    const cont = el.nextElementSibling;
-    const icon = el.lastElementChild;
-
-    console.log(cont, icon)
-
-    cont.classList.toggle('view');
-    icon.classList.toggle('view')
 
 }
